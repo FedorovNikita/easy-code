6445456ac65d8948d024cef37ac16e1855582b66
Fedorov Nikita <fedorovni1996@yandex.ru>
2021-11-17 22:43:19 +0500

    add lesson17

---
+++ part1/lesson17/app.js
@@ -0,0 +1,10 @@
+function foo(x) {
+  console.log(x);
+  console.log(arguments);
+
+  for (let i = 0; i < arguments.length; i++) {
+    console.log(arguments[i]);
+  }
+}
+
+foo(11, "some string", [1, 2, 3])
