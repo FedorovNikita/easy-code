f21b69022d362c51577618dbbbbe5f5ac6e76ade
Nikita Fedorov <fedorovni1996@yandex.ru>
2021-11-18 09:59:14 +0500

    add solve task to lesson17

--- part1/lesson17/homework/main.js
+++ part1/lesson17/homework/main.js
@@ -59,3 +59,34 @@
 // console.log(getCodeStringFromText(null));
 // console.log(getCodeStringFromText(undefined));
 
+// 4. Создать функцию угадай число. 
+// Она принимает число от 1-10 (обязательно проверить чито число не больше 10 и не меньше 0)
+// Если число не соответствует условию то верните ошибку return new Error("Please provide number in range 0 - 10")
+// Если передано не число то верните ошибку return new Error("Please provide a valid number")
+// Далее функция генерирует рандомное число от 1-10 и сравнивает с заданным числом если они совпали, то возвращает строку "You win!"
+// Если нет, то  строку "You are lose, your number is 8, the random number is 5". Числа в строке указаны как пример. Вы подставляете реальные числа
+// Если передано число в виде строки оно должно быть преобразовано к числу
+
+function guessTheNumber(num) {
+
+    num = Number(num);
+    
+    if (typeof num !== "number" || isNaN(num)) {
+        return new Error("Please provide a valid number");
+    }
+
+    if (num < 0 || num > 10) {
+        return new Error("Please provide number in range 0 - 10");
+    }
+
+    let randomValue = Math.ceil(Math.random() * 10);
+    
+    if (randomValue === num) {
+        return "You win!";
+    } else {
+        return `You are lose, your number is ${num}, the random number is ${randomValue}`;
+    }
+
+}
+
+console.log(guessTheNumber(4));
